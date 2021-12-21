8f6eb7eb79cbb8c27f4d9edd05e8bc450b6fd5c7
DanielDS03 <dandorsan@alum.us.es>
2021-12-21 15:09:37 +0100

    Modificado requirements.txt para despliegue

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -70,7 +71,7 @@ MODULES = [
     'voting',
 ]
 
-BASEURL = 'https://equipo1decide.herokuapp.com/'
+BASEURL = 'https://equipo10decide.herokuapp.com/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -113,7 +114,7 @@ DATABASES = {
         'USER': 'decide',
         'PASSWORD': 'decide',
         'HOST': 'localhost',
-        'PORT': '5432',
+        'PORT': '5435',
     }
 }
 
@@ -180,3 +181,4 @@ if os.path.exists("config.jsonnet"):
 
 
 INSTALLED_APPS = INSTALLED_APPS + MODULES
+django_heroku.settings(locals())
