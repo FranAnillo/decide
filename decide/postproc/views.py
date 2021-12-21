72d052f832b9431dcf1df4732af62849c468f433
dierodcam <dierodcam@alum.us.es>
2021-12-21 23:12:13 +0100

    Creacion del algoritmo de votacion relativa

--- decide/postproc/views.py
+++ decide/postproc/views.py
@@ -16,9 +16,51 @@ class PostProcView(APIView):
         out.sort(key=lambda x: -x['postproc'])
         return Response(out)
 
-    def post(self, request):
+
+def relativa(self, options):
+        out= []
+        numvotos=0
+
+        for opt in options:
+            numvotos=opt['votes']+numvotos
+            out.append({
+                **opt,
+                'postproc':0,
+            })
+
+        mayor=0.0
+        list=out.copy()
+        while len(list)>=2:
+
+            if len(list)>2:
+                cocientes = []
+                for i in range(len(list)):
+                   cocientes.append(list[i]['votes']/numvotos)       
+                perdedor=cocientes.index(min(cocientes))
+                ganador=cocientes.index(max(cocientes))
+                mayor=cocientes[ganador]
+                if mayor>0.5:
+                    g=list[ganador]['number']
+                    out[g-1]['postproc']= 1
+                    break
+                numvotos= numvotos - cocientes[perdedor]
+                del list[perdedor]
+            elif len(list)==2:
+                cocientes = []
+                for i in range(len(list)):
+                    cocientes.append(list[i]['votes']/numvotos)
+                ganador=cocientes.index(max(cocientes)) 
+                g=list[ganador]['number'] 
+                out[g-1]['postproc']= 1
+                break
+        out.sort(key=lambda x:-x['votes'])
+        return Response(out)
+
+
+
+def post(self, request):
         """
-         * type: IDENTITY | EQUALITY | WEIGHT
+         * type: IDENTITY | DHONT | RELATIVA | ABSOLUTA | BORDA | SUBTRAC
          * options: [
             {
              option: str,
@@ -27,12 +69,31 @@ class PostProcView(APIView):
              ...extraparams
             }
            ]
+        * seats: int
         """
 
-        t = request.data.get('type', 'IDENTITY')
+        t = request.data.get('type')
         opts = request.data.get('options', [])
+        order_opts = request.data.get('order_options', [])
+        s = request.data.get('seats')
+        p = request.data.get('paridad')
+
+        if len(opts) == 0 and len(order_opts) == 0:
+            return Response({}, status=status.HTTP_400_BAD_REQUEST)
 
         if t == 'IDENTITY':
             return self.identity(opts)
-
-        return Response({})
+        elif t == 'RELATIVA':
+            return self.relativa(opts)
+        elif t == 'ABSOLUTA':
+            return self.absoluta(opts)
+        elif t == 'BORDA':
+            if len(order_opts) == 0:
+                return Response({}, status=status.HTTP_400_BAD_REQUEST)
+            else:
+                return self.borda(order_opts)
+        elif t == 'SUBTRAC':
+            if (s == None):
+                return Response({}, status=status.HTTP_400_BAD_REQUEST)
+            else:
+                return Response(self.subtrac(opts, s))
