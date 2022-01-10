8ecf4458274377fde5937d8f51cd53926588e6fd
AlvaroAg1995 <alvalcgra@alum.us.es>
2022-01-10 18:31:26 +0100

    Prueba para el boton de compartir resultados

--- decide/visualizer/tests.py
+++ decide/visualizer/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
 # Create your tests here.
+def test_facebook(self):
+	self.driver.get(https://equipo1decide.herokuapp.com/visualizer/1/)
+	self.driver.set_window_size(1298, 863)
+	self.driver.find_elements(By.CSS_SELECTOR, "a:nth-child(2) > img").click()
+	assert self.driver.find_elements(By.ID, "homelink").text == "Facebook"
