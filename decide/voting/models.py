2fdc5022f7cf63d038650ed21eb2836267bd9839
FranAnillo <60961813+FranAnillo@users.noreply.github.com>
2022-01-10 23:01:03 +0100

    Cambio model.py
    eliminación de código correcto por error

--- decide/voting/models.py
+++ decide/voting/models.py
@@ -171,6 +171,18 @@ class Voting(models.Model):
     question = models.ForeignKey(Question, related_name='voting', on_delete=models.CASCADE)
     seats = models.PositiveIntegerField(default=1)
 
+    VOTING_TYPE_OPTIONS = [
+        ('IDENTITY', 'IDENTITY'),
+        ('WEBSTER', 'WEBSTER'),
+        ('DHONT', 'DHONT'),
+        ('RECUENTO_BORDA', 'RECUENTO_BORDA'),
+        ('RELATIVA', 'RELATIVA'),
+        ('MAYORIA_ABSOLUTA', 'MAYORIA_ABSOLUTA'),
+        ('HAMILTON', 'HAMILTON'),
+        ('SUBTRAC', 'SUBTRAC')]
+
+    voting_type= models.CharField(max_length=50,choices=VOTING_TYPE_OPTIONS,default='IDENTITY')
+    
     start_date = models.DateTimeField(blank=True, null=True)
     end_date = models.DateTimeField(blank=True, null=True)
 
